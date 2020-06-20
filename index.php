89e12e96b39d5de577e568b072e0bbadbdb10404
Gillyprmna <67154428+Gillyprmna@users.noreply.github.com>
2020-06-21 01:06:01 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,107 @@
+GILLY WAS HIRR :v
+<link href="https://i.ibb.co/LgGWrsr/20200619-093745.jpg" rel="icon" type="image/x-icon"/>
+<body><iframe width="0" height="0" scrolling="no" frameborder="no" allow="autoplay" src="https://e.top4top.io/m_1630gw6vv0.mp3" ></iframe></body>
+<title>RAWA RONTEK X-PLOIT</title>
+<script src="https://cdn.rawgit.com/FicriPebriyana/efek/0a935a6c/efek%20salju.js" type="text/javascript"></script>
+<script language="javascript">alert("Lu nyolong sc gua , gua pukul lu");</script>
+<meta content='TOUCH BY GILLY' name='description'/>
+<meta content='SAVE!' name='keywords'/>
+<meta content='' name='Abstract'/>
+</head>
+<style type="text/css">
+body {
+ background-color:black;
+	color:red;
+	font-family:monospace;
+	font-size:14px;
+}
+img {
+	opacity:0.5;
+}
+img:hover {
+	opacity:1;
+}
+a {
+	text-decoration:none;
+	color:#FFFFFF;
+}
+a:hover {
+	text-decoration:none;
+	color:red;
+}
+x{
+	color: white;
+}
+@keyframes fade {
+    from { opacity: 1.0; }
+    50% { opacity: 0.5; }
+    to { opacity: 1.0; }
+}                                                                                                                                                                                                                                  
+
+@-webkit-keyframes fade {
+    from { opacity: 1.0; }
+    50% { opacity: 0.5; }
+    to { opacity: 1.0; }
+}
+
+.blink {
+  animation:fade 0.5s infinite;
+  -webkit-animation:fade 0.2s infinite;
+}
+</style>
+<link href='https://fonts.googleapis.com/css?family=Iceland|Shadows+Into+Light&display=swap' rel='stylesheet' type='text/css'>
+<script type="text/javascript">
+var uid = '113084';
+var wid = '226606';
+</script>
+<body> 
+<center>
+<table height="0%" width="0%">
+	<tr>
+		<td align="center">
+		<br><br><br><br><br><br><center>
+		<h2>
+		<table><table><tr><td><img src="https://a.top4top.io/p_1630xjbz99.png'" widht="500" height="500"></td><td>
+		<font face="iceland" size="5" color="red">MY FRIENDS<center></center></h2><h6></font><span style="syntax">
+		<font face="iceland" size="5" color="white">
+		* WAEN<br>
+		* XOYEN<br>
+		* HBAE<br>
+		* SASKE<br>
+		* DEDE AL<br>
+		* NOVAL<br>
+		* VN9<br>
+		* TIGER<br>
+		* IAMRAFIQ<br>
+		<font face="iceland" class="" size="5" color="white">* FDEX</font><br>
+		* OBITOD<br>
+		* RAMLAN<br>
+		* B4T0S4Y<br>
+		* MR.F<br>
+		* NZUL<br>
+		* HASBY<br>
+		* TARIGAN<br>
+		* LION<br>
+		* ALCATRAS<br>
+		
+		</h6></font><br></td></tr></table>
+<font color="white" class="blink" face="iceland" size="10">[ Touch By Gilly ]</font><br>
+<b><font class="blink" face="iceland" size="20"> RAWA RONTEK <x> X-PLOIT </x></font></b>
+<br>
+<b><font class="blink" face="iceland" size="5"> FROM DORK <x> TO XPLOIT </x></font></b>
+<br/><br>
+<font class="blink" color="white" face="iceland"size="5">MESSAGE:</font><br>
+<font color="red" face="Shadows Into Light"size="5"> Maaf tidak bisa seperti<x> yang kamu inginkan </font><br>
+<font color="red" face="Shadows Into Light"size="5">Aku hanya mencintaimu<x> dengan caraku:)</font><br>
+	<i><i><x></x><x></i><x>
+	<marquee behavior="blink" direction="left" width="500" scrollamount="50" >_____________________________</marquee></br>
+			<font face="iceland" size="5">>MY FAMILY<<x></x></font> 
+			<font  size="5"></font><br><br>
+<marquee width="50%">
+	<x>[RAWA RONTEK X-PLOIT] [JAKARTA CYBER TEAM OFC] [UZUMAKI X-PLOIT] [WE-TOXIC 2019]<x></marquee>
+<br/>
+<marquee behavior="blink" direction="right" width="500" scrollamount="50" >_____________________________</marquee><br><br>
+<audio autoplay="autoplay" controls="controls" src="https://k.top4top.io/m_1624jp80h0.mp3" type="audio/mpeg"></audio><br><br>
+<br><center><font face="iceland"  color="red" size="4">Follow me on youtube</font><br><br><a href='https://www.youtube.com/c/GILLYGANS'> <img alt='Youtube' src='https://www.upload.ee/image/11226706/images__5_-removebg-preview.png' width='40' height='40'></a> </footer></div>
+</body>
+</html>
